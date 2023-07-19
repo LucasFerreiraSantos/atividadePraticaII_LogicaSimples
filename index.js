aa43c02f72f6a5baa43e4829b61b1313cfc82252
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-19 19:38:04 -0300

    feito o exercício I e iniciado o II

---
+++ index.js
@@ -0,0 +1,7 @@
+// Exercício I
+let tempCelsius = 26
+let tempFahrenheit
+tempFahrenheit = tempCelsius * 1.8 + 32
+document.write(`A temperatura Celsius é de ${tempCelsius}, sua conversão para Fahrenheit é ${tempFahrenheit.toFixed(2)}.`)
+
+// Exercício II
