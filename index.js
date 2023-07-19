187f675f39aabb6a0b86cfdf3b87b09b31c62612
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-19 20:56:25 -0300

    feita a estrutura do exer5

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 let tempCelsius = 26
 let tempFahrenheit
 tempFahrenheit = tempCelsius * 1.8 + 32
-document.write(`A temperatura Celsius é de ${tempCelsius}, sua conversão para Fahrenheit é ${tempFahrenheit.toFixed(2)}.`)
+document.write(`<p>A temperatura Celsius é de ${tempCelsius}, sua conversão para Fahrenheit é ${tempFahrenheit.toFixed(2)}.</p>`)
 
 // Exercício IV
+const valorCarro = 56000
+const taxaDistribuicao = (28 / 100) * valorCarro
+const taxaImpostos = (45 / 100) * valorCarro
+document.write(`<p>A taxa de distribuição é de R$${taxaDistribuicao.toFixed(2)}. A taxa de impostos é de R$${taxaImpostos.toFixed(2)} e o valor de fábrica do veículo é de R$${valorCarro - taxaDistribuicao - taxaImpostos}</p>`)
