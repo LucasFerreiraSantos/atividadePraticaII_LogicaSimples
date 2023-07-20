a848448b66a448344b084b4f7656b24dbc188191
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-20 19:19:35 -0300

    feito o exerc5

--- exer5/imc.js
+++ exer5/imc.js
@@ -1,11 +1,11 @@
-const altura = parseFloat(prompt('Digite a sua altura: '))
-const peso = parseFloat(prompt('Digite o seu peso: '))
-const imc = peso / altura * altura
+const altura = prompt('Digite a sua altura: ')
+const peso = prompt('Digite o seu peso: ')
+const imc = peso / (altura * altura)
     if(imc < 18.5){
-        document.write(`<p>Você está abaixo da faixa de peso ideal.</p>`)
+        document.write(`<p>Seu IMC é de ${imc.toFixed(2)}. Você está abaixo da faixa de peso ideal.</p>`)
     } else if(imc > 24.99){
-        document.write(`<p>Você está acima da faixa de peso ideal.</p>`)
+        document.write(`<p>Seu IMC é de ${imc.toFixed(2)}. Você está acima da faixa de peso ideal.</p>`)
     }else{
-        document.write(`<p>Você está dentro da faixa
+        document.write(`<p>Seu IMC é de ${imc.toFixed(2)}. Você está dentro da faixa
         de peso ideal.</p>`)
     }
